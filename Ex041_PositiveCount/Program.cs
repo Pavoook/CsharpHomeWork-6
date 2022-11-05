67f471f47c3b10190ecb334daf6fe0b593aa6b67
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-06 01:41:25 +0500

    Added Exersize №41 Positive Count

---
+++ Ex041_PositiveCount/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] array = GetArrayFromString(AskAndGetString("Введите несколько целых чисел через запятую:"));
+int counter = 0;
+foreach (int i in array)
+{
+    if (i > 0) counter++;
+}
+Console.WriteLine($"Количество введенных положительных чисел равно {counter}.");
+
+string AskAndGetString(string message)
+{
+    Console.WriteLine(message);
+    string inputString = Console.ReadLine();
+    return inputString;
+}
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numbers = stringArray.Split(",", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numbers.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result[i] = int.Parse(numbers[i]);
+    }
+    return result;
+}
