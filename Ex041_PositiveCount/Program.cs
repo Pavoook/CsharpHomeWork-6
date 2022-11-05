70b92289504bbcd437a05530d0e55e6a4999c5ab
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-06 02:41:56 +0500

    Corrected Exersize №41 Positive Count

--- Ex041_PositiveCount/Program.cs
+++ Ex041_PositiveCount/Program.cs
@@ -4,12 +4,8 @@
 // -1, -7, 567, 89, 223-> 3
 
 int[] array = GetArrayFromString(AskAndGetString("Введите несколько целых чисел через запятую:"));
-int counter = 0;
-foreach (int i in array)
-{
-    if (i > 0) counter++;
-}
-Console.WriteLine($"Количество введенных положительных чисел равно {counter}.");
+int positiveCount = GetPositiveCount(array);
+Console.WriteLine($"Количество введенных положительных чисел равно {positiveCount}.");
 
 string AskAndGetString(string message)
 {
@@ -28,4 +24,14 @@ int[] GetArrayFromString(string stringArray)
         result[i] = int.Parse(numbers[i]);
     }
     return result;
+}
+
+int GetPositiveCount(int[] collection)
+{
+    int counter = 0;
+    foreach (int i in collection)
+    {
+        if (i > 0) counter++;
+    }
+    return counter;
 }
